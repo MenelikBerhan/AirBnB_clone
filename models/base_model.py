54b442c5558db5b0fc8178bcf089988c3415522d
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-09 05:17:59 +0300

    Add BaseModel class for tasks 3 and 4

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Contains the `BaseModel` class"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel():
+    """A base class that defines common attributes and methods
+    for other classes.
+    """
+    def __init__(self, *args, **kwargs):
+        """Creates a `BaseModel` instance object"""
+        if kwargs:
+            for key, value in kwargs.items():
+                # if key == '__class__':
+                #     continue
+                if key in ('created_at', 'updated_at'):
+                    self.__setattr__(key, datetime.fromisoformat(value))
+                elif key != '__class__':
+                    self.__setattr__(key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """`str`: returns a fancier string representation"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def save(self):
+        """Updates `updated_at` instance attribute with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """`dict`: returns a dictionary containing all keys/values of
+        `__dict__` of the instance"""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return (dict_copy)
