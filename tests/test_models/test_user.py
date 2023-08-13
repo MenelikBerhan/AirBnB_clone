e886b6ed04230cf15a365627c422ffcecc3b7349
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-13 15:09:00 +0300

    Add doc test for User

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -156,6 +156,11 @@ class TestUser(unittest.TestCase):
             obj_dict['updated_at'] = obj.updated_at.isoformat()
             self.assertEqual(obj.to_dict(), obj_dict)
 
+    def test_doc(self):
+        """Tests presence of documentation"""
+        self.assertIsNotNone(__import__('models.user').__doc__)
+        self.assertIsNotNone(User.__doc__)
+
 
 if __name__ == '__main__':
     unittest.main()
